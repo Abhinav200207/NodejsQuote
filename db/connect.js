48bd10f443d7f514c90d2993a908f97d93b37d3a
Abhinav200207 <82315896+Abhinav200207@users.noreply.github.com>
2022-01-07 16:05:38 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-MONGO_URI = 'mongodb+srv://Abhinav:12345@cluster0.ipq1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://Abhinav:<NotKnown>@cluster0.ipq1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const connectdb = () => {
     return mongoose.connect(MONGO_URI)
